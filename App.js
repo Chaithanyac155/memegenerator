cd5b3a457061af5c1b1f81c9694e413d8aec1be9
Chaithanya Kumar <68162279+Chaithanyac155@users.noreply.github.com>
2021-06-29 14:45:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDOM from "react-dom"
 import './App.css';
-import Form from './Form';
-import Login from './Login';
 import Header from './MemeGen/Header';
 import MemeGenerator from './MemeGen/MemeGenerator';
 import './MemeGen/Meme.css';
@@ -10,8 +7,6 @@ import './MemeGen/Meme.css';
 function App() {
   return (
     <div className="App">
-      {/* <Form /> */}
-        {/* <Login /> */}
  
         <Header />
         <MemeGenerator />
